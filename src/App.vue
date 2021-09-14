0d441e3c847bc8892d3a838559f8038c700a2333
roobel2005 <roobel2005@gmail.com>
2021-09-14 23:57:56 +0300

    added template

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <div></div>
+  <div style="width: 100vw; height: 100vh">
+    <img :src="template" style="width: 100%; height: 100%" />
+  </div>
 </template>
 
 <script>
+import template from "./templates/banana.png";
 export default {
-  name: 'App'
-}
+  name: "App",
+  data() {
+    return {
+      template: template,
+    };
+  },
+};
 </script>
-
-
