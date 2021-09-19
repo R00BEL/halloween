ff994e9add5b8ae6275a559dcd8bdacccee9829a
roobel2005 <roobel2005@gmail.com>
2021-09-19 11:19:33 +0300

    added registration Images

--- src/server.js
+++ src/server.js
@@ -11,16 +11,47 @@ const getUser = async (accessToken) => {
   const response = await fetch("https://api.linkedin.com/v2/me", {
     headers: {
       Authorization: `Bearer ${accessToken}`,
-      "Content-Type": "application/json",
     },
   });
   return response.json();
 };
 
+const registerAnUploadForImages = async (accessToken, userId) => {
+  const body = {
+    registerUploadRequest: {
+      owner: `urn:li:person:${userId}`,
+      recipes: ["urn:li:digitalmediaRecipe:feedshare-image"],
+      serviceRelationships: [
+        {
+          identifier: "urn:li:userGeneratedContent",
+          relationshipType: "OWNER",
+        },
+      ],
+      supportedUploadMechanism: ["SYNCHRONOUS_UPLOAD"],
+    },
+  };
+  const response = await fetch(
+    "https://api.linkedin.com/v2/assets?action=registerUpload",
+    {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
+  return response.json();
+};
+
 app.post("/", async (req, res) => {
-  const accessToken = "1111111111";
+  const accessToken = "11111111";
   const user = await getUser(accessToken);
-  console.log(user.id);
+  const registeredPicture = await registerAnUploadForImages(
+    accessToken,
+    user.id
+  );
+  console.log(registeredPicture);
 });
 
 app.listen(3000);
